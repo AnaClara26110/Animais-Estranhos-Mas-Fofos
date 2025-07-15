1cb4f50a224a82d8485ff98c4d9c9982bdc01d9f
AnaClara26110 <anaclara26110@gmail.com>
2025-07-15 16:01:00 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+// Script para abrir e fechar a barra lateral
+function w3_open() {
+    document.getElementById("mySidebar").style.display = "block";
+}
+
+function w3_close() {
+    document.getElementById("mySidebar").style.display = "none";
+}
